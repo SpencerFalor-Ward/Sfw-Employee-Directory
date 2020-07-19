baa29ec5ae6db0a4174af196e2765652d5791b58
SpencerFalor-Ward <srfalorward@ucdavis.edu>
2020-07-19 01:19:47 -0700

    trying to fix deployment issue

---
+++ build/precache-manifest.891a232665a52ecee7bcb49fa4d7e0b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a660a71cb6ea0e0604af60ac5c22cc9",
+    "url": "/Sfw-Employee-Directory/index.html"
+  },
+  {
+    "revision": "1f23df7a12f4b23a5422",
+    "url": "/Sfw-Employee-Directory/static/css/main.840a4475.chunk.css"
+  },
+  {
+    "revision": "2770ea2977141a6a5e8f",
+    "url": "/Sfw-Employee-Directory/static/js/2.ff288ef3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Sfw-Employee-Directory/static/js/2.ff288ef3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f23df7a12f4b23a5422",
+    "url": "/Sfw-Employee-Directory/static/js/main.041a4cc0.chunk.js"
+  },
+  {
+    "revision": "f235a126a5ab1ae25baf",
+    "url": "/Sfw-Employee-Directory/static/js/runtime-main.745f2ecf.js"
+  }
+]);
